c53f278d28db69650c452c1f652ca05e7b6bfd03
ChWick <ch.wick@web.de>
2013-12-28 17:56:18 +0100

    added diploma font

--- Game/GUIInstructions.cpp
+++ Game/GUIInstructions.cpp
@@ -15,6 +15,8 @@ CGUIInstructions::CGUIInstructions(CEGUI::Window *pGUIRoot) {
   using namespace CEGUI;
 
   ImageManager::getSingleton().loadImageset("instr.imageset");
+  FontManager::getSingleton().createFreeTypeFont("diploma15", 15, true, "diploma.ttf" );
+  FontManager::getSingleton().createFreeTypeFont("diploma20", 20, true, "diploma.ttf" );
 
   m_pInstructionsRoot = pGUIRoot->createChild("OgreTray/StaticImage", "Instructions/Root");
   m_pInstructionsRoot->setInheritsAlpha(false);
@@ -24,11 +26,20 @@ CGUIInstructions::CGUIInstructions(CEGUI::Window *pGUIRoot) {
   m_pInstructionsRoot->setProperty("FrameEnabled","False");
   m_pInstructionsRoot->setProperty("BackgroundEnabled","False");
 
+  Window *pInsTitle = m_pInstructionsRoot->createChild("OgreTray/StaticText", "Instructions/Root/Text");
+  pInsTitle->setPosition(UVector2(UDim(0.35, 0), UDim(0.13, 0)));
+  pInsTitle->setSize(USize(UDim(0.4, 0), UDim(0.1, 0)));
+  pInsTitle->setProperty("FrameEnabled","False");
+  pInsTitle->setProperty("BackgroundEnabled","False");
+  pInsTitle->setText("Anweisungen:");
+  pInsTitle->setFont("diploma20");
+
   m_pText = m_pInstructionsRoot->createChild("OgreTray/StaticText", "Instructions/Root/Text");
-  m_pText->setPosition(UVector2(UDim(0.3, 0), UDim(0.3, 0)));
-  m_pText->setSize(USize(UDim(0.4, 0), UDim(0.4, 0)));
+  m_pText->setPosition(UVector2(UDim(0.3, 0), UDim(0.17, 0)));
+  m_pText->setSize(USize(UDim(0.4, 0), UDim(0.6, 0)));
   m_pText->setProperty("FrameEnabled","False");
   m_pText->setProperty("BackgroundEnabled","False");
+  m_pText->setFont("diploma15");
 
 
   hide();
