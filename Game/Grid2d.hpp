ac89ed0de2ac8122deaea52ca0d22a2f03445497
ChWick <ch.wick@web.de>
2013-12-31 14:33:43 +0100

    using scope operators for std

--- Game/Grid2d.hpp
+++ Game/Grid2d.hpp
@@ -4,25 +4,25 @@
 #include <vector>
 
 template <class T>
-class grid2d : public vector<T> {
+class grid2d : public std::vector<T> {
 private:
-  size_t sizeX;
-  size_t sizeY;
+  std::size_t sizeX;
+  std::size_t sizeY;
 
 public:
-  
-  size_t getSizeX() const {return sizeX;}
-  size_t getSizeY() const {return sizeY;}
 
-  void resize(size_t x, size_t y) {
+  std::size_t getSizeX() const {return sizeX;}
+  std::size_t getSizeY() const {return sizeY;}
+
+  void resize(std::size_t x, std::size_t y) {
     sizeX = x;
     sizeY = y;
-    vector<T>::resize(x * y);
+    std::vector<T>::resize(x * y);
   }
-  const T &operator()(size_t x, size_t y) const {
+  const T &operator()(std::size_t x, std::size_t y) const {
     return (*this)[sizeY * x + y];
   }
-  T &operator()(size_t x, size_t y) {
+  T &operator()(std::size_t x, std::size_t y) {
     return (*this)[sizeY * x + y];
   }
 };
