b6f98de86ecd96e34e70904b78e9c90608562902
C. Wick <ch.wick@web.de>
2014-03-11 16:24:11 +0100

    added separated debug defines for different playforms

--- Game/DebugDefines.hpp
+++ Game/DebugDefines.hpp
@@ -1,8 +1,10 @@
 #ifndef DEBUG_DEFINES_H
 #define DEBUG_DEFINES_H
 
-
-// #define DEBUG_SHOW_OGRE_TRAY
+#ifdef ANDROID
+#define DEBUG_SHOW_OGRE_TRAY
+#else
+#define DEBUG_SHOW_OGRE_TRAY
 #define DEBUG_COLLISION_WITH_TILES
 #define DEBUG_CHARACTER_BOUNDING_BOXES
 #define DEBUG_ENDANGERED_TILES
@@ -10,6 +12,6 @@
 #define DEBUG_KI
 #define DEBUG_EXIT
 #define DEBUG_PLAYER_NO_COLLISION
-
+#endif
 
 #endif
