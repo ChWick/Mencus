3248f3921ab439f65bb78b5e05cac643964450f8
C. Wick <ch.wick@web.de>
2014-03-11 16:28:30 +0100

    updates for android, no debug output

--- Game/DebugDefines.hpp
+++ Game/DebugDefines.hpp
@@ -2,8 +2,11 @@
 #define DEBUG_DEFINES_H
 
 #ifdef ANDROID
-#define DEBUG_SHOW_OGRE_TRAY
+
+//#define DEBUG_SHOW_OGRE_TRAY
+
 #else
+
 #define DEBUG_SHOW_OGRE_TRAY
 #define DEBUG_COLLISION_WITH_TILES
 #define DEBUG_CHARACTER_BOUNDING_BOXES
@@ -12,6 +15,7 @@
 #define DEBUG_KI
 #define DEBUG_EXIT
 #define DEBUG_PLAYER_NO_COLLISION
+
 #endif
 
 #endif
