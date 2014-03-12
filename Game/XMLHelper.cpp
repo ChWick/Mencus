027e576cd5834083c9b03e3bd303ddc5eb587dcc
C. Wick <ch.wick@web.de>
2014-03-12 13:45:03 +0100

    added vector2, real, and string parsing

--- Game/XMLHelper.cpp
+++ Game/XMLHelper.cpp
@@ -13,4 +13,25 @@ namespace XMLHelper {
       return bDefault;
     }
   }
+
+  Ogre::Real RealAttribute(tinyxml2::XMLElement *pElem,
+			   const char *pLabel,
+			   Ogre::Real fDefault) {
+    if (pElem->Attribute(pLabel)) {
+      return pElem->FloatAttribute(pLabel);
+    }
+    return fDefault;
+  }
+  Ogre::String Attribute(tinyxml2::XMLElement *pElem, const char *pLabel, const Ogre::String &sDefault) {
+    assert(pElem);
+    assert(pLabel);
+    if (pElem->Attribute(pLabel)) {return Ogre::String(pElem->Attribute(pLabel));}
+    return sDefault;
+  }
+  Ogre::Vector2 Vector2Attribute(tinyxml2::XMLElement *pElem,
+				 const Ogre::String &prefix,
+				 const Ogre::Vector2 &vDefault) {
+    return Ogre::Vector2(RealAttribute(pElem, (prefix + "x").c_str(), vDefault.x),
+			 RealAttribute(pElem, (prefix + "y").c_str(), vDefault.y));
+  }
 };
--- Game/XMLHelper.hpp
+++ Game/XMLHelper.hpp
@@ -4,15 +4,35 @@
 #include <tinyxml2.h>
 #include <assert.h>
 #include <string>
+#include <OgreString.h>
+#include <OgreVector2.h>
 
 namespace XMLHelper {
-  bool BoolAttribute(tinyxml2::XMLElement *pElem, const char *pLabel, bool bDefault);
+  bool BoolAttribute(tinyxml2::XMLElement *pElem,
+		     const char *pLabel,
+		     bool bDefault = true);
+
+  Ogre::Real RealAttribute(tinyxml2::XMLElement *pElem,
+			   const char *pLabel,
+			   Ogre::Real fDefault = 0);
+
+  Ogre::String Attribute(tinyxml2::XMLElement *pElem,
+			 const char *pLabel,
+			 const Ogre::String &sDefault = Ogre::StringUtil::BLANK);
+
+  Ogre::Vector2 Vector2Attribute(tinyxml2::XMLElement *pElem,
+				 const Ogre::String &prefix = Ogre::StringUtil::BLANK,
+				 const Ogre::Vector2 &vDefault = Ogre::Vector2::ZERO);
+
   template <class T>
-  T EnumAttribute(tinyxml2::XMLElement *pElem, const char *pLabel, T eDefault) {
+  T EnumAttribute(tinyxml2::XMLElement *pElem,
+		  const char *pLabel,
+		  T eDefault,
+		  int iOffset = 0) {
     assert(pElem);
     assert(pLabel);
     if (!pElem->Attribute(pLabel)) {return eDefault;}
-    return static_cast<T>(pElem->IntAttribute(pLabel));
+    return static_cast<T>(pElem->IntAttribute(pLabel) + iOffset);
   }
 
 };
