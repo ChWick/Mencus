e38abe9d17cc9d7a06c6a0e70540bac402e6708d
C. Wick <ch.wick@web.de>
2014-03-12 13:26:17 +0100

    added convert funtions for link direction

--- Game/Link.hpp
+++ Game/Link.hpp
@@ -11,6 +11,23 @@ public:
     LD_SECOND_TO_FIRST    = 2,
     LD_BOTH               = 3
   };
+
+  static std::string toString(ELinkDirection eLD) {
+    switch (eLD) {
+    case LD_FIRST_TO_SECOND:
+      return "first_to_second";
+    case LD_SECOND_TO_FIRST:
+      return "second_to_first";
+    default:
+      return "both";
+    }
+  }
+  static ELinkDirection parseLinkDirection(const std::string &str) {
+    if (str == "first_to_second") {return LD_FIRST_TO_SECOND;}
+    if (str == "second_to_first") {return LD_SECOND_TO_FIRST;}
+    if (str == "both") {return LD_BOTH;}
+    throw Ogre::Exception(0, "Invalid direction in link: " + str, __FILE__);
+  }
 private:
   unsigned int m_uiFirstX;
   unsigned int m_uiFirstY;
