007bd43e764efee934c2405cd7b919b0dea190e7
ChWick <ch.wick@web.de>
2014-03-13 12:11:27 +0100

    added const modifier and added parsing functions

--- Game/XMLHelper.cpp
+++ Game/XMLHelper.cpp
@@ -1,7 +1,10 @@
 #include "XMLHelper.hpp"
 
 namespace XMLHelper {
-  bool BoolAttribute(tinyxml2::XMLElement *pElem, const char *pLabel, bool bDefault)
+  bool BoolAttribute(const tinyxml2::XMLElement *pElem,
+		     const char *pLabel,
+		     bool bDefault,
+		     bool bRequired)
     {
     assert(pElem);
     assert(pLabel);
@@ -10,29 +13,64 @@ namespace XMLHelper {
       return label == "true" || label == "True" || label == "1";
     }
     else {
+      if (bRequired) {
+	throw Ogre::Exception(0,
+			      "Required attribute '" + Ogre::String(pLabel) + "' not found",
+			      __FILE__);
+      }
       return bDefault;
     }
   }
 
-  Ogre::Real RealAttribute(tinyxml2::XMLElement *pElem,
+  Ogre::Real RealAttribute(const tinyxml2::XMLElement *pElem,
 			   const char *pLabel,
-			   Ogre::Real fDefault) {
+			   Ogre::Real fDefault,
+			   bool bRequired) {
     if (pElem->Attribute(pLabel)) {
       return pElem->FloatAttribute(pLabel);
     }
+    if (bRequired) {
+      throw Ogre::Exception(0,
+			    "Required attribute '" + Ogre::String(pLabel) + "' not found",
+			    __FILE__);
+    }
     return fDefault;
   }
-  Ogre::String Attribute(tinyxml2::XMLElement *pElem, const char *pLabel, const Ogre::String &sDefault) {
+  int IntAttribute(const tinyxml2::XMLElement *pElem,
+		   const char *pLabel,
+		   int iDefault,
+		   bool bRequired) {
+    if (pElem->Attribute(pLabel)) {
+      return pElem->IntAttribute(pLabel);
+    }
+    if (bRequired) {
+      throw Ogre::Exception(0,
+			    "Required attribute '" + Ogre::String(pLabel) + "' not found",
+			    __FILE__);
+    }
+    return iDefault;
+  }
+
+  Ogre::String Attribute(const tinyxml2::XMLElement *pElem,
+			 const char *pLabel,
+			 const Ogre::String &sDefault,
+			 const  bool bRequired) {
     assert(pElem);
     assert(pLabel);
     if (pElem->Attribute(pLabel)) {return Ogre::String(pElem->Attribute(pLabel));}
+    if (bRequired) {
+      throw Ogre::Exception(0,
+			    "Required attribute '" + Ogre::String(pLabel) + "' not found",
+			    __FILE__);
+    }
     return sDefault;
   }
-  Ogre::Vector2 Vector2Attribute(tinyxml2::XMLElement *pElem,
+  Ogre::Vector2 Vector2Attribute(const tinyxml2::XMLElement *pElem,
 				 const Ogre::String &prefix,
-				 const Ogre::Vector2 &vDefault) {
-    return Ogre::Vector2(RealAttribute(pElem, (prefix + "x").c_str(), vDefault.x),
-			 RealAttribute(pElem, (prefix + "y").c_str(), vDefault.y));
+				 const Ogre::Vector2 &vDefault,
+				 bool bRequired) {
+    return Ogre::Vector2(RealAttribute(pElem, (prefix + "x").c_str(), vDefault.x, bRequired),
+			 RealAttribute(pElem, (prefix + "y").c_str(), vDefault.y, bRequired));
   }
 
 
--- Game/XMLHelper.hpp
+++ Game/XMLHelper.hpp
@@ -7,33 +7,58 @@
 #include <OgreString.h>
 #include <OgreStringConverter.h>
 #include <OgreVector2.h>
+#include <OgreException.h>
 
 namespace XMLHelper {
-  bool BoolAttribute(tinyxml2::XMLElement *pElem,
+  bool BoolAttribute(const tinyxml2::XMLElement *pElem,
 		     const char *pLabel,
-		     bool bDefault = true);
+		     bool bDefault = true,
+		     bool bRequired = false);
 
-  Ogre::Real RealAttribute(tinyxml2::XMLElement *pElem,
+  Ogre::Real RealAttribute(const tinyxml2::XMLElement *pElem,
 			   const char *pLabel,
-			   Ogre::Real fDefault = 0);
+			   Ogre::Real fDefault = 0,
+			   bool bRequired = false);
 
-  Ogre::String Attribute(tinyxml2::XMLElement *pElem,
+  int IntAttribute(const tinyxml2::XMLElement *pElem,
+		   const char *pLabel,
+		   int iDefault = 0,
+		   bool bRequired = false);
+
+  Ogre::String Attribute(const tinyxml2::XMLElement *pElem,
 			 const char *pLabel,
-			 const Ogre::String &sDefault = Ogre::StringUtil::BLANK);
+			 const Ogre::String &sDefault = Ogre::StringUtil::BLANK,
+			 bool bRequired = false);
 
-  Ogre::Vector2 Vector2Attribute(tinyxml2::XMLElement *pElem,
+  Ogre::Vector2 Vector2Attribute(const tinyxml2::XMLElement *pElem,
 				 const Ogre::String &prefix = Ogre::StringUtil::BLANK,
-				 const Ogre::Vector2 &vDefault = Ogre::Vector2::ZERO);
+				 const Ogre::Vector2 &vDefault = Ogre::Vector2::ZERO,
+				 bool bRequired = false);
 
   template <class T>
-  T EnumAttribute(tinyxml2::XMLElement *pElem,
+  T EnumAttribute(const tinyxml2::XMLElement *pElem,
 		  const char *pLabel,
 		  T eDefault,
-		  int iOffset = 0) {
+		  int iOffset = 0,
+		  bool bRequired = false) {
     assert(pElem);
     assert(pLabel);
-    if (!pElem->Attribute(pLabel)) {return eDefault;}
+    if (!pElem->Attribute(pLabel)) {
+      if (bRequired) {
+	throw Ogre::Exception(0,
+			      "Required attribute '" + Ogre::String(pLabel) + "' not found",
+			      __FILE__);
+      }
+      return eDefault;
+    }
     return static_cast<T>(pElem->IntAttribute(pLabel) + iOffset);
+  }
+  template <class T>
+  T EnumAttribute(const tinyxml2::XMLElement *pElem,
+		  const char *pLabel,
+		  T eDefault,
+		  bool bRequired) {
+    return EnumAttribute<T>(pElem, pLabel, eDefault, 0, bRequired);
   }
 
 
