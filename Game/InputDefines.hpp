39b022f5895c7a327ffbbfc8d8a3f663cd830263
C. Wick <ch.wick@web.de>
2014-03-25 11:13:25 +0100

    if enabled map editor, mouse is required on non touch devices;

--- Game/InputDefines.hpp
+++ Game/InputDefines.hpp
@@ -15,10 +15,14 @@
 #  define INPUT_KEYBOARD
 
 #else
-//#  define INPUT_TOUCH
-//#  define INPUT_MOUSE
-#  define INPUT_KEYBOARD
-#  define INPUT_KEYBOARD_ONLY
+
+#  ifdef MAP_EDITOR_ENABLED
+#    define INPUT_MOUSE
+#    define INPUT_KEYBOARD
+#  else
+#    define INPUT_KEYBOARD
+#    define INPUT_KEYBOARD_ONLY
+#  endif
 
 #endif
 
