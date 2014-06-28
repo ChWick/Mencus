b7e2c66eba099336c907dc5525153633d5c73346
C. Wick <ch.wick@web.de>
2014-06-28 13:54:40 +0200

    fixed wrong spelling on include the shared library of amazon games

--- cmake/toolchain/AmazonGameCircle.cmake
+++ cmake/toolchain/AmazonGameCircle.cmake
@@ -1,7 +1,7 @@
 if (MENCUS_USE_AMAZON_GAME_CIRCLE) 
   set(MENCUS_AMAZON_GAME_CIRCLE_ANDROID_MK 
 "include $(CLEAR_VARS)
-LOCAL_MODULE := AmazonGameJni
+LOCAL_MODULE := AmazonGamesJni
 LOCAL_SRC_FILES := libs/libAmazonGamesJni.so
 include $(PREBUILT_SHARED_LIBRARY)")
 
