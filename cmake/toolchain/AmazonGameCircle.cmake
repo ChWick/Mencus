15105579957584c506a5623a59a052ed2fe4519c
C. Wick <ch.wick@web.de>
2014-06-29 18:03:25 +0200

    added amazon game circles activity

--- cmake/toolchain/AmazonGameCircle.cmake
+++ cmake/toolchain/AmazonGameCircle.cmake
@@ -6,4 +6,32 @@ LOCAL_SRC_FILES := libs/libAmazonGamesJni.so
 include $(PREBUILT_SHARED_LIBRARY)")
 
   set(MENCUS_AMAZON_GAME_CIRCLE_LIBRARY "AmazonGamesJni")
+
+  set(MENCUS_AMAZON_GAME_CIRCLE_ACTIVITY
+ "<activity android:name=\"com.amazon.ags.html5.overlay.GameCircleUserInterface\"
+	      android:theme=\"@style/GCOverlay\" android:hardwareAccelerated=\"false\"></activity>
+    <activity
+	android:name=\"com.amazon.identity.auth.device.authorization.AuthorizationActivity\"
+	android:theme=\"@android:style/Theme.NoDisplay\"
+	android:allowTaskReparenting=\"true\"
+	android:launchMode=\"singleTask\">
+      <intent-filter>
+	<action android:name=\"android.intent.action.VIEW\" />
+	<category android:name=\"android.intent.category.DEFAULT\" />
+	<category android:name=\"android.intent.category.BROWSABLE\" />
+	<data android:host=\"org.mencus.game\" android:scheme=\"amzn\" />
+      </intent-filter>
+    </activity>
+    <activity android:name=\"com.amazon.ags.html5.overlay.GameCircleAlertUserInterface\"
+	      android:theme=\"@style/GCAlert\" android:hardwareAccelerated=\"false\"></activity>
+    <receiver
+	android:name=\"com.amazon.identity.auth.device.authorization.PackageIntentReceiver\"
+	android:enabled=\"true\">
+      <intent-filter>
+	<action android:name=\"android.intent.action.PACKAGE_INSTALL\" />
+	<action android:name=\"android.intent.action.PACKAGE_ADDED\" />
+	<data android:scheme=\"package\" />
+      </intent-filter>
+    </receiver>"
+)
 endif()
