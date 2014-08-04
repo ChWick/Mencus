fa5dd92b0c0f3e04dc87d43e8ccbc1dd110ec408
C. Wick <ch.wick@web.de>
2014-08-04 17:05:23 +0200

    fixes for MacOS

--- Game/XMLResources/Manager.cpp
+++ Game/XMLResources/Manager.cpp
@@ -23,7 +23,7 @@
 namespace XMLResources {
   std::string CManager::LANGUAGE_CODE;
 
-#if OGRE_PLATFORM == OGRE_PLATFORM_ANDROID
+#if OGRE_PLATFORM == OGRE_PLATFORM_ANDROID || OGRE_PLATFORM == OGRE_PLATFORM_APPLE || OGRE_PLATFORM == OGRE_PLATFORM_APPLE_IOS
   CManager GLOBAL("", "language/", false);
 #else
   CManager GLOBAL("", "../language/", false);
